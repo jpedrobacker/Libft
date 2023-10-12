cfd87c78b3a2cd8ee36a9d0334c1f698d728aa59
JP <jpedrobacker@outlook.com>
2023-10-12 18:03:52 -0300

    ft_strtrim commit

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,22 +1,62 @@
 #include "libft.h"
 
-static size_t check_char(char const c, char  *s1)
+static int	ft_char_is_true(char c, char const *set)
 {
-	while (*s1)
+	size_t i;
+
+	i = 0;
+	while (set[i])
 	{
-		if (c == *s1)
+		if (set[i] == c)
 			return (1);
-		s1++;
+		i++;
 	}
 	return (0);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+static size_t ft_start_str(char const *s1, char const *set)
 {
-  
+	size_t start;
+
+	start = 0;
+	while (s1[start] && ft_char_is_true(s1[start], set))
+		start++;
+	return (start);
 }
 
+static size_t ft_end_str(char const *s1, char const *set, size_t start)
+{
+	size_t end;
+
+	end = ft_strlen(s1);
+	while (end > start && ft_char_is_true(s1[end - 1], set))
+		end--;
+	return (end);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	size_t	i;
+	size_t	start;
+	size_t	end;
+
+	start = ft_start_str(s1, set);
+	end = ft_end_str(s1, set, start);
+	str = (char *)malloc(sizeof(*s1) * (end - start + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = 0;
+	return (str);
+}
+
+/*
 int main(void)
 {
-  
+	printf("%s\n", ft_strtrim(".-.-.ola---", ".-"));
+	return (0);
 }
+*/
--- libft.h
+++ libft.h
@@ -34,6 +34,7 @@ char	*ft_strnstr(const char *dst, const char *src, size_t len);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *string);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(const char *s, char c);
 
 #endif
