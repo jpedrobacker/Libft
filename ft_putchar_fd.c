c47decfd66266ad2d222e5d18cbbe78955cf8b51
Joao Pedro Bergfeld Backer Lopes <jpedrobacker@MacBook-Air-de-Paula.local>
2023-10-14 15:22:31 -0300

    ft_putchar_fd fix

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,6 +1,13 @@
 #include "libft.h"
 
-void ft_putchar_fd(char c, int fd)
+void	ft_putchar_fd(char c, int fd)
 {
-  write(fd, &c, 1);
+	write(fd, &c, 1);
 }
+
+/*
+int main(void)
+{
+	ft_putchar_fd('c', 1);
+}
+*/
