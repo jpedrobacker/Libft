ded6e02a35a86c972e4f03d6f2493ff5631dbda7
Joao Pedro Bergfeld Backer Lopes <jpedrobacker@MacBook-Air-de-Paula.local>
2023-10-14 14:20:23 -0300

    ft_tolower.c fix

--- ft_tolower.c
+++ ft_tolower.c
@@ -2,24 +2,21 @@
 
 int ft_tolower(int c)
 {
-  if (c >= 65 && c <= 90)
-  {
-    c += 32;
+    if (c >= 65 && c <= 90)
+        c += 32;
     return (c);
-  }
 }
 
 /*
 int main(void)
 {
-  char str[] = "OLA";
-  int i = 0;
-  
-  while (str[i] != '\0')
-  {
-    printf("%c", ft_tolower(str[i]));
-    i++;
-  }
-  printf("\n");
+    char str[] = "OLA";
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        printf("%c", ft_tolower(str[i]));
+        i++;
+    }
+    printf("\n");
 }
 */
