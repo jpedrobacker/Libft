c788fe67ac23e32f0653d0c34cdd764ae3fdfa76
JP <jpedrobacker@outlook.com>
2023-10-14 12:25:50 -0300

    ft_strlcat norm fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,38 +1,33 @@
 #include "libft.h"
 
-size_t ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-  size_t i;
-  size_t j;
-  size_t value;
+	size_t	i;
+	size_t	j;
+	size_t	value;
 
-  i = 0;
-  j = 0;
-  if (size == 0)
-    return (ft_strlen(src));
-  else if (size < ft_strlen(dst))
-    value = ft_strlen(src) + size;
-  else
-    value = ft_strlen(src) + ft_strlen(dst);
-  while (dst[i] != '\0')
-    i++;
-  while (src[j] != '\0' && j < size)
-  {
-    dst[i] = src[j];
-    i++;
-    j++;
-  }
-  dst[i] = '\0';
-  return (i);
+	i = 0;
+	j = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	else if (size < ft_strlen(dst))
+		value = ft_strlen(src) + size;
+	else
+		value = ft_strlen(src) + ft_strlen(dst);
+		while (dst[i] != '\0')
+			i++;
+		while (src[j] != '\0' && j < size)
+			dst[i++] = src[j++];
+		dst[i] = '\0';
+		return (i);
 }
-
 /*
 int main(void)
 {
-  char dst[] = "junta essa string";
-  char src[100] = "com essa outra string";
-  
-  ft_strlcat(dst, src, 10);
-  printf("%s",dst);
+	char dst[] = "junta essa string";
+	char src[100] = " com essa outra string";
+
+	ft_strlcat(dst, src, ft_strlen(src));
+	printf("%s",dst);
 }
 */
