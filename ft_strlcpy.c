3051f9ac7d8ad4d1b872370a4b676a47a5849e26
JP <jpedrobacker@outlook.com>
2023-10-14 12:32:45 -0300

    ft_strlcpy norm fix

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,21 +1,23 @@
 #include "libft.h"
 
-size_t  ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-  size_t i = 0;
-  size_t j = 0;
-  
-  while (src[i] != '\0')
-  {
-    if (size && (i < (size - 1)))
-    {
-      dest[i] = src[i];
-      j++;
-    }
-    i++;
-  }
-  dest[j] = '\0';
-  return (i);
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (src[i] != '\0')
+	{
+		if (size && (i < (size - 1)))
+		{
+			dst[i] = src[i];
+			j++;
+		}
+		i++;
+	}
+	dst[j] = '\0';
+	return (i);
 }
 
 /*
