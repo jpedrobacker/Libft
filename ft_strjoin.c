3707b8880fb2157e2437b124a38bf2d126e406b0
JP <jpedrobacker@outlook.com>
2023-10-14 12:15:02 -0300

    ft_strjoin norm fix

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,18 +3,18 @@
 char *ft_strjoin(char const *s1, char const *s2)
 {
 	int	i;
-  int lens1;
-  int lens2;
-	char *str;
+	int	lens1;
+	int	lens2;
+	char	*str;
 
 	i = 0;
-  lens1 = ft_strlen(s1);
-  lens2 = ft_strlen(s2);
-  str = (char *)malloc(lens1 + lens2 + 1);
-  if (s1 == 0 && s2 == 0)
-    return (0);
-  if (!str)
-    return (0);
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	str = (char *)malloc(lens1 + lens2 + 1);
+	if (s1 == 0 && s2 == 0)
+		return (0);
+	if (!str)
+		return (0);
 	while (i < lens1 || i < lens2)
 	{
 		if (i < lens1)
@@ -32,7 +32,7 @@ int main(void)
 {
   char *s1 = "ola";
   char *s2 = "+";
-  
+
 	printf("%s", ft_strjoin(s1, s2));
 }
 */
