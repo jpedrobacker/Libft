118df72aff06a1ed09fc288031a9dc340a07533a
JP <jpedrobacker@outlook.com>
2023-10-14 18:02:05 -0300

    ft_calloc fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,38 +1,38 @@
 #include "libft.h"
 
-void *ft_calloc(size_t nitems, size_t size)
+void	*ft_calloc(size_t nitems, size_t size)
 {
-  size_t j;
-  int *i;
-  
-  if (nitems == 0 || size == 0)
-  {
-    nitems = 1;
-    size = 1;
-  }
-  j = nitems * size;
-  i = malloc(j);
-  if (!i)
-    return (NULL);
-  else
-    ft_bzero(i, j);
-  return (i);
+	size_t	j;
+	int	*i;
+
+	if (nitems == 0 || size == 0)
+	{
+		nitems = 1;
+		size = 1;
+	}
+	j = nitems * size;
+	if (!(i = malloc(j)))
+		return (NULL);
+	else
+		ft_bzero(i, j);
+	return (i);
 }
 
 /*
 int main(void)
 {
-  int *ptr;
-  int i;
-  int n;
-  
-  ptr = (int*)ft_calloc(n, sizeof(int));
-  for (i = 0; i < n; ++i)
-  {
-    ptr[i] = i + 1;
-  }
-	for (i = 0; i < n; ++i)
-  {
+	int i = 0;
+	int n = 0;
+	int *ptr = (int *)ft_calloc(n, sizeof(int));
+
+	while (i < n)
+	{
+		ptr[i] = i + 1;
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
 		printf("%d, ", ptr[i]);
 	}
 }
