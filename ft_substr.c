c1226e22bc87e7e25f4b9cef3b9270c7b535657a
JP <jpedrobacker@outlook.com>
2023-10-14 13:41:06 -0300

    ft_substr fix?

--- ft_substr.c
+++ ft_substr.c
@@ -1,35 +1,36 @@
 #include "libft.h"
 
-char *ft_substr(char const *src, unsigned int start, size_t len)
+char	*ft_substr(const char *src, unsigned int start, size_t len)
 {
-  int i;
-  int l;
-  char *dst;
-  char *s;
+	int	i;
+	int	l;
+	char	*dst;
+	char	*s;
 
-  s = (char *)src;
-  l = len - start;
-  dst = (char *)malloc(sizeof(char) * (l + 1));
-  i = start;
-  while (i < len && (*(s + i) != '\0'))
-  {
-    *dst = *(s + i);
-    dst++;
-    i++;
-  }
-  *dst = '\0';
-  return (dst - l);
+	s = (char *)src;
+	l = len - start;
+	i = start;
+	if (!(dst = (char *)malloc(sizeof(char) * (l + 1))))
+		return (NULL);
+	while (i < len && (*(s + i) != '\0'))
+	{
+		*dst = *(s + i);
+		dst++;
+		i++;
+	}
+	*dst = '\0';
+	return (dst - l);
 }
 
 /*
 int main(void)
 {
-  char src[] = "substr funcao implementacao";
-  int m = 7;
-  int n = 12;
+	char src[] = "substr funcao implementacao";
+	int m = 7;
+	int n = 12;
 
-  char *dest = ft_substr(src, m, n);
-  printf("%s\n", dest);
-  return (0);
+	char *dest = ft_substr(src, m, n);
+	printf("%s\n", dest);
+	return (0);
 }
 */
