ee600aa928b9703f8dafb9b90cf955235188f053
JP <jpedrobacker@outlook.com>
2023-10-14 18:07:40 -0300

    ft_memset fix

--- ft_memset.c
+++ ft_memset.c
@@ -1,22 +1,22 @@
 #include "libft.h"
 
-void *ft_memset(char *str, int a, size_t n)
+void	*ft_memset(char *str, int a, size_t n)
 {
-  size_t i;
+	size_t	i;
 
-  i = 0;
-  while (i < n)
-    {
-      str[i] = a;
-      i++;
-    }
+	i = 0;
+	while (i < n)
+	{
+		str[i] = a;
+		i++;
+	}
 }
 
 /*
 int main(void)
 {
-  char str[] = "olha essa string com isso aqui";
-  ft_memset(str, '&', 4);
-  printf("%s", str);
+	char *str = "olha essa string com isso aqui";
+	ft_memset(str, '&', 4);
+	printf("%s", str);
 }
 */
