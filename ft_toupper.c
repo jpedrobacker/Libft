615908f0d32d9a1e8dcbc09782906f19b2642ed7
Joao Pedro Bergfeld Backer Lopes <jpedrobacker@MacBook-Air-de-Paula.local>
2023-10-14 14:12:56 -0300

    ft_toupper.c fix

--- ft_toupper.c
+++ ft_toupper.c
@@ -2,24 +2,21 @@
 
 int ft_toupper(int c)
 {
-  if (c >= 97 && c <= 122)
-  {
-    c -= 32;
+    if (c >= 97 && c <= 122)
+        c -= 32;
     return (c);
-  }
 }
 
 /*
 int main(void)
 {
-  char str[] = "ola";
-  int i = 0;
-  
-  while (str[i] != '\0')
-  {
-    printf("%c", ft_toupper(str[i]));
-    i++;
-  }
-  printf("\n");
+    char str[] = "ola";
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        printf("%c", ft_toupper(str[i]));
+        i++;
+    }
+    printf("\n");
 }
 */
