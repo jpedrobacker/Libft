c69af30717c5599c4561e8c4142bd9b5f524073f
JP <jpedrobacker@outlook.com>
2023-10-14 21:33:40 -0300

    ft_isalpha fix

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,21 +1,16 @@
 #include "libft.h"
 
-int ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
-  if (c >= 65 && c <= 90)
-    return (1);
-  else if (c >= 97 && c <= 122)
-    return (1);
-  else
-    return (0);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
 }
 
 /*
 int main(void)
 {
-  char c;
-
-  c = 'q';
-  printf("%d", ft_isalpha(c));
+	printf("%d", ft_isalpha('q'));
 }
 */
