176e61e0594a7b008b17fab0ea3e986feecfa2e4
JP <jpedrobacker@outlook.com>
2023-10-15 12:12:34 -0300

    ft_strlcat fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -4,16 +4,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	j;
-	size_t	value;
 
 	i = 0;
 	j = 0;
 	if (size == 0)
 		return (ft_strlen(src));
 	else if (size < ft_strlen(dst))
-		value = ft_strlen(src) + size;
+		return (ft_strlen(src) + size);
 	else
-		value = ft_strlen(src) + ft_strlen(dst);
 		while (dst[i] != '\0')
 			i++;
 		while (src[j] != '\0' && j < size)
@@ -21,10 +19,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 		dst[i] = '\0';
 		return (i);
 }
+
 /*
 int main(void)
 {
-	char dst[] = "junta essa string";
+	char dst[100] = "junta essa string";
 	char src[100] = " com essa outra string";
 
 	ft_strlcat(dst, src, ft_strlen(src));
