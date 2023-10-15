60938d500d15bb23b00745bf87253216aa30e401
JP <jpedrobacker@outlook.com>
2023-10-14 21:36:17 -0300

    ft_isascii fix

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,19 +1,16 @@
 #include "libft.h"
 
-int ft_isascii(int c)
+int	ft_isascii(int c)
 {
-  if (c >= 0 && c <= 127)
-    return (1);
-  else
-    return (0);
+	if (c >= 0 && c <= 127)
+		return (1);
+	else
+		return (0);
 }
 
 /*
 int main(void)
 {
-  char c;
-
-  c = 'q';
-  printf("%d", ft_isascii(c));
+	printf("%d", ft_isascii('q'));
 }
 */
