6ed0c612ef9bd8ab7637b19c5bf9cca0ee755eef
JP <jpedrobacker@outlook.com>
2023-10-15 12:09:00 -0300

    ft_strnstr fix

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -11,7 +11,7 @@ char *ft_strnstr(const char *str, const char *to_find, size_t n)
 	while (str[i] != '\0')
 	{
 		j = 0;
-		while (str[i + j] != '\0' && str[i + j] == to_find[j])
+		while (str[i + j] != '\0' && str[i + j] == to_find[j] && i + j < n)
 		{
 			if (to_find[j + 1] == '\0')
 				return ((char *)str + i);
