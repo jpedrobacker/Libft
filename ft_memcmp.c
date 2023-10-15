66adb2a78f5eead06119acfa8d1d444938388b4f
JP <jpedrobacker@outlook.com>
2023-10-14 21:49:52 -0300

    ft_memcmp fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,27 +1,29 @@
 #include "libft.h"
 
-int ft_memcmp(const void *str1, const void *str2, size_t n)
+int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-  size_t i;
-  char *istr1 = (char *)str1;
-  char *istr2 = (char *)str2;
+	size_t i;
+	char *istr1;
+	char *istr2;
 
-  i = 0;
-  while (i < n)
-  {
-    if (istr1[i] < istr2[i] || istr1[i] > istr2[i])
-      return (istr1[i] - istr2[i]);
-    i++;
-  }
-  return (0);
+	istr1 = (char *)str1;
+	istr2 = (char *)str2;
+	i = 0;
+	while (i < n)
+	{
+		if (istr1[i] < istr2[i] || istr1[i] > istr2[i])
+			return (istr1[i] - istr2[i]);
+		i++;
+	}
+	return (0);
 }
 
 /*
 int main(void)
 {
-  char str1[] = "a";
-  char str2[] = "a";
-  
-  printf("%d",ft_memcmp(str1, str2, 1));
+	char *str1 = "a";
+	char *str2 = "z";
+
+	printf("%d", ft_memcmp(str1, str2, 1));
 }
 */
