44acd8a7e37daa00f257da0e6b779b68aa1b81c5
JP <jpedrobacker@outlook.com>
2023-10-14 22:01:07 -0300

    ft_memmove fix

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,27 +1,30 @@
 #include "libft.h"
 
-void *ft_memmove(void *str1, const void *str2, size_t n)
+void	*ft_memmove(void *str1, const void *str2, size_t n)
 {
-  char *dstr1 = (char *)str1; //dest string
-  char *dstr2 = (char *)str2; //src string
-  size_t i;
+	char *dstr1;
+	char *dstr2;
+	size_t i;
 
-  i = 0;
-  while (dstr2[i] != '\0' && i < n)
-    {
-      dstr1[i] = dstr2[i];
-      i++;
-    }
+	i = 0;
+	dstr1 = (char *)str1;
+	dstr2 = (char *)str2;
+
+	while (dstr2[i] != '\0' && i < n)
+	{
+		dstr1[i] = dstr2[i];
+		i++;
+	}
 }
 
 /*
 int main(void)
 {
-  char dest[] = "ola";
-  const char src[]  = "como esta";
-  
-  printf("Before memmove dest = %s, src = %s\n", dest, src);
-  ft_memmove(dest, src, 4);
-  printf("After memmove dest = %s, src = %s\n", dest, src);
+	char *dst = "ola";
+	const char *src = " como esta";
+
+	printf("Antes memmove dest = %s, src = %s\n", dst, src);
+	ft_memmove(dst, src, 4);
+	printf("Depois memmove dest = %s, src = %s\n", dst, src);
 }
 */
