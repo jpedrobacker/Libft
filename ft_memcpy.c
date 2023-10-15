48111966b2a668a2e613d4309031472ac04adde9
JP <jpedrobacker@outlook.com>
2023-10-14 21:56:05 -0300

    ft_memcpy fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,26 +1,28 @@
 #include "libft.h"
 
-void *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-    char *csrc = (char *)src; 
-    char *cdst = (char *)dst; 
-    size_t i;
-    
-    i = 0;
-    while (csrc[i] != '\0' && i < n)
-    {
-        cdst[i] = csrc[i];
-        i++;
-    }
+	char	*csrc;
+	char	*cdst;
+	size_t	i;
+
+	i = 0;
+	cdst = (char *)dst;
+	csrc = (char *)src;
+	while (csrc[i] != '\0' && i < n)
+	{
+		cdst[i] = csrc[i];
+		i++;
+	}
 }
 
 /*
-int main(void)
+int main (void)
 {
-    char src[50] = "copia essa string";
-    char dst[50];
+	char *src = "copia essa string";
+	char *dst;
 
-    ft_memcpy(dst, src, strlen(src) + 1);
-    printf("%s", dst);
+	ft_memcpy(dst, src, strlen(src) + 1);
+	printf("%s", dst);
 }
 */
