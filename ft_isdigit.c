cea149c1edf77d81619aaca2dfb70a53c4d0fc62
JP <jpedrobacker@outlook.com>
2023-10-14 21:38:40 -0300

    ft_isdigit fix

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,19 +1,16 @@
 #include "libft.h"
 
-int ft_isdigit(int c)
+int	ft_isdigit(int c)
 {
-  if (c >= 48 && c <= 57)
-    return (1);
-  else
-    return (0);
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
 }
 
 /*
 int main(void)
 {
-  char c; 
-
-  c = 'q';
-  printf("%d", ft_isdigit(c));
+	printf("%d", ft_isdigit('q'));
 }
 */
