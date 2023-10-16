e87e78da3bc2d0d24223f6ff52ea72b6edc57e62
jbergfel <jpedrobacker@outlook.com>
2023-10-16 19:42:15 -0300

    ft_strlcat a mexer

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jbergfel <jbergfel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 10:39:01 by jbergfel          #+#    #+#             */
-/*   Updated: 2023/10/16 10:39:02 by jbergfel         ###   ########.fr       */
+/*   Updated: 2023/10/16 19:37:53 by jbergfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,24 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	j;
+	size_t	h;
 
-	i = 0;
+	i = ft_strlen(src);
 	j = 0;
-	if (size == 0)
-		return (ft_strlen(src));
-	else if (size < ft_strlen(dst))
-		return (ft_strlen(src) + size);
-	else
+	while (j < size && dst[j])
+		j++;
+	h = j;
+	while (src[j - h] && (j + 1) < size)
 	{
-		while (dst[i] != '\0')
-			i++;
-		while (src[j] != '\0' && j < size)
-			dst[i++] = src[j++];
-		dst[i] = '\0';
-		return (i);
+		dst[j] = src[j - h];
+		j++;
 	}
+	if (h < size)
+		dst[j] = '\0';
+	return (h + i);
 }
 
-/*
+
 int main(void)
 {
 	char dst[100] = "junta essa string";
@@ -43,4 +42,4 @@ int main(void)
 	ft_strlcat(dst, src, ft_strlen(src));
 	printf("%s",dst);
 }
-*/
+
