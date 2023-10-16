3ede32d976209a31aa20fe46488dd33ce0fb8581
jbergfel <jpedrobacker@outlook.com>
2023-10-16 14:55:22 -0300

    ft_putnbr_fd ok

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jbergfel <jbergfel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 10:38:38 by jbergfel          #+#    #+#             */
-/*   Updated: 2023/10/16 10:38:39 by jbergfel         ###   ########.fr       */
+/*   Updated: 2023/10/16 14:54:33 by jbergfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,18 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 	{
-		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(2, fd);
-		ft_putnbr_fd(147483648, fd);
+		ft_putstr_fd("-2147483648", fd);
+		return ;
 	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(n, fd);
+		n *= -1;
 	}
-	else if (n > 9)
+	if (n > 9)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
 	else
 		ft_putchar_fd(n + 48, fd);
