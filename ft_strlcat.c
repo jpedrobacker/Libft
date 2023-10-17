472dd2d35a2c6f0b39a32b739bb9c53175591298
jbergfel <jpedrobacker@outlook.com>
2023-10-17 10:40:56 -0300

    ft_strlcat ok

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jbergfel <jbergfel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 10:39:01 by jbergfel          #+#    #+#             */
-/*   Updated: 2023/10/16 19:37:53 by jbergfel         ###   ########.fr       */
+/*   Updated: 2023/10/17 10:39:58 by jbergfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	i = 0;
 	dstlen = ft_strlen(dst);
 	srclen = ft_strlen(src);
-	if (!size)
-		return (srclen);
+	if (dstlen > size)
+		return (srclen + size);
 	while (src[i] && dstlen + i < size - 1)
 	{
 		dst[dstlen + i] = src[i];
 		i++;
 	}
 	dst[dstlen + i] = '\0';
-	if (dstlen > size)
-		return (srclen + size);
 	return (srclen + dstlen);
 }
 
@@ -40,7 +38,7 @@ int main(void)
 	char dst[30] = "123";
 	char src[30] = "456";
 
-	ft_strlcat(dst, src, 10);
-	printf("%d",ft_strlcat(dst, src, ft_strlen(src)));
+	ft_strlcat(dst, src, 2);
+	printf("%ld",ft_strlcat(dst, src, 2));
 }
 */
