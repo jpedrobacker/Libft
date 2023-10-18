27e2f207876d7d05c0d701464bb3bd9ca3e1f880
JP <jpedrobacker@outlook.com>
2023-10-18 18:43:50 -0300

    Lista bonus começo

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -4,19 +4,38 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list *new;
 
-	new = (t_list *)malloc(sizeof(t_list));
+	new = (t_list *)malloc(sizeof(t_list)); // aloca o espaço necessario de cada node
 	if (!new)
 		return (NULL);
-	new->content = content;
-	new->next = NULL;
+	new->content = content;// joga o node na lista
+	new->next = NULL;// bota o null depois para informar que é o fim da lista
 	return (new);
 }
 
 /*
+void printlist(t_list *head)
+{
+	t_list *temp = head;
+
+	while (temp != NULL)
+	{
+		printf("%d - ", temp->content);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+
 int main(void)
 {
-	printf("%s\n", ft_lstnew("Hello")->content);
+	t_list *temp;
+	t_list *head = NULL;
 
-	return(0);
+	for (void *x = 20; x > 0; x--)
+	{
+		temp = ft_lstnew(x); // cria um novo node
+		temp->next = head; // linka o novo node a lista atual
+		head = temp; // joga o ultimo node criado no head da lista
+	}
+	printlist(head);
 }
 */
